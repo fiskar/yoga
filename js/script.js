d3ac5a8d6de1ef863a129b79e8466348f769d2c0
fiskar <artem@hismatullin.ru>
2020-05-16 21:10:20 +0500

    Popup addded

--- js/script.js
+++ js/script.js
@@ -82,4 +82,31 @@ window.addEventListener("DOMContentLoaded", function () {
 		}
 	}
 	setClock("timer", deadline);
+
+	//Modal
+
+	let more = document.querySelector('.more'),
+	overlay = document.querySelector('.overlay'),
+	close = document.querySelector('.popup-close'),
+	description= document.querySelectorAll('.description-btn');
+
+	more.addEventListener('click', function() {
+		overlay.style.display = 'block';
+		this.classList.add('more-splash');
+		document.body.style.overflow = 'hidden';
+	});
+	close.addEventListener('click', function() {
+		overlay.style.display = 'none';
+		more.classList.remove('more-splash');
+		document.body.style.overflow = '';
+	});
+	for (let i=0; i<description.length; i++) {
+		description[i].addEventListener('click', function() {
+			overlay.style.display = 'block';
+			this.classList.add('more-splash');
+			document.body.style.overflow = 'hidden';
+		});
+	}
+	
+	
 });
